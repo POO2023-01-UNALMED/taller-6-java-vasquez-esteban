4d3743099136a758cb14dd9cbe11c8d0c31a68e3
vasquez-esteban <124936891+vasquez-esteban@users.noreply.github.com>
2023-03-25 09:53:58 -0500

    Update Camioneta.java

--- src/main/java/Camioneta.java
+++ src/main/java/Camioneta.java
@@ -6,7 +6,7 @@ public class Camioneta extends Vehiculo {
 	
 	public Camioneta(String placa, int puertas, String nombre,
 			int precio, int peso, Fabricante fabricante, boolean volco) {
-		super(placa , puertas, 90, nombre, precio, peso, "4x4", fabricante);
+		super(placa , puertas, 90, nombre, precio, peso, "4X4", fabricante);
 		this.setVolco(volco);
 		cantidadCamionetas++;
 		
